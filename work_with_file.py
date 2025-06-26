785cd87b8a8de8d0f52d45195beacffa1fd68c3d
thorenda <thorenda@student.21-school.ru>
2025-06-26 12:16:54 +0300

    Добавил к флагу --where реализацию символов < >

--- work_with_file.py
+++ work_with_file.py
@@ -6,14 +6,21 @@ def read_with_where(file_name: str, where_arg: list[str]):
     file_list = []
     with open(file_name, "r", encoding="utf-8") as file:
         table_from_file = csv.reader(file)
+        header = file.readline().lstrip('\n').rstrip('\n').split(',')
+        file_list.append(header)
+        index = header.index(where_arg[0])
         for s in table_from_file:
             if where_arg[1] == "=":
-                for i in s:
-                    if i == where_arg[2] or i == where_arg[0]:
-                        file_list.append(s)
-            if where_arg[1] == "<":
-                pass
-        
+                if where_arg[2].lower() in [i.lower() for i in s] and where_arg[0] not in s:
+                    print([i.lower() for i in s])
+                    file_list.append(s)
+            elif where_arg[1] == "<":
+                if s[index].isdigit and float(s[index]) < float(where_arg[2]):
+                    file_list.append(s)
+            elif where_arg[1] == ">":
+                if s[index].isdigit and float(s[index]) > float(where_arg[2]):
+                    file_list.append(s)
+
     print(output_format(file_list))
 
 
